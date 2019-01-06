8e6e9e6b83eb4f5220ccb05c6296e2c7b0a533d8
Diogo dos Santos <diogo.santos@celsolisboa.edu.br>
2019-01-05 22:57:08 -0200

    s3bucket criado para guardar access key

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "firstdemo" {
-    ami           = "ami-922914f7"
-    instance_type = "t2.micro"
-
-tags {
-    Name = "demoinstance"
-}
-
-}
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    access_key = "AKIAJJBDFYXI7L7VPBSQ"
-    secret_key = "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" #AWS secret acess key
-    region     = "us-east-2" #region in wich you want to operate
-}
