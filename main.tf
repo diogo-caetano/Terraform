84ea3d6c2b846737ff5d923fab53f18b3a15215c
diogo-caetano <matrixeoMelhorfilme>
2019-01-08 13:51:43 -0200

    mudando para visual studio code no pc do trabalho

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "firstdemo" {
-    ami           = "ami-922914f7"
+    count         = "1"
+    ami           = "ami-922914f7" #tem que ser feito na us-east-2 por conta da restrição da AMI
     instance_type = "t2.micro"
 
 tags {
-    Name = "demoinstance"
+    Name = "demoinstance-${count.index}"
 }
 
 }
